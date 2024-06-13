4f396888a9056eda17957e378b76dbf5bfca5c9b
Madhukar <madhukar@xpresslane.in>
2024-06-13 11:34:37 +0530

    Nabar, Enums, useEffect, customHook

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,36 @@
+import Navbar from "@/scenes/navbar";
+import { useEffect, useState } from "react";
+
+import { SelectedPage } from "@/shared/types";
+
 export default function App() {
-  return <div className="bg-gray-20">App</div>;
+  // const [selectedPage, setSelectedPage] = useState('home');
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home,
+  );
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      } else if (window.scrollY !== 0) {
+        setIsTopOfPage(false);
+      }
+      console.log(isTopOfPage);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <div className="app bg-gray-20">
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+    </div>
+  );
 }
